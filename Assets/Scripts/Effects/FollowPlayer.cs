566055a8e3ed0135946e559fd19865920e3096a2
Edgar Alexandro Castillo Palacios <a00830568@tec.mx>
2023-11-28 10:31:21 -0600

    Particle system
    Se agregaron sistemas de partículas al tocar agua y lava y para los spawners

---
+++ Assets/Scripts/Effects/FollowPlayer.cs
@@ -0,0 +1,34 @@
+/* Function: allows the particle effects to follow the player
+   Author: Edgar Alexandro Castillo Palacios
+   Modification date: 28/11/2023 */
+
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5f;
+
+    void Awake()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject player in players)
+        {
+            if (player.transform.position == transform.position)
+            {
+                target = player.transform;
+                break;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            if(target.GetComponent<PlayerControl>().moveSpeed >= 5) Destroy(gameObject);
+        }
+    }
+}
